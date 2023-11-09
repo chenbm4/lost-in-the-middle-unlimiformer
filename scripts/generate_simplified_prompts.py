74189b3633f126f5cb53d97157d56e6c94d79e3c
Brian Chen <chenbm4@gmail.com>
2023-11-09 05:24:25 -0500

    Update generate_simplified_prompts.py

--- scripts/generate_simplified_prompts.py
+++ scripts/generate_simplified_prompts.py
@@ -141,8 +141,8 @@ def format_instruct_prompt(instruction):
 
 def format_chat_prompt(instruction):
     # Format the prompt according to LLaMa 2 chat model requirements
-    prompt = f"<s>[INST] <<SYS>>\n Below is an instruction that describes a task. \\
-        Write a response that appropriately completes the request. \n<</SYS>>\n\n{instruction}"
+    prompt = f"<s>[INST] <<SYS>>\n Below is an instruction that describes a task." \
+        "Write a response that appropriately completes the request. \n<</SYS>>\n\n{instruction}"
     return prompt
 
 if __name__ == "__main__":
